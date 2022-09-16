c5379297098d154efe4a1950aa3d3f4eded021b4
sfcc5555 <sfcc.5555@gmail.com>
2022-09-16 14:14:05 -0500

    Actualización

--- static/YARDSALE/yardsale.js
+++ static/YARDSALE/yardsale.js
@@ -1,13 +1,33 @@
 let cm=document.getElementById("contenedorMenu");
 let cmv=document.getElementById("contenedorMenuMovile");
+let moc=document.getElementById("contenedormisordenes");
 
 function mostrarMenu() {
-    cm.classList.toggle("menu-container")
+    cm.classList.toggle("menu-container");
+    if (moc.className=="none myOrderContainer")
+    {
+        moc.classList.toggle("myOrderContainer");
+    }
 }
 
 
 function mostrarMenuMovile() {
-    cmv.classList.toggle("menu-movile-container")
+    cmv.classList.toggle("menu-movile-container");
+    if (moc.className=="none myOrderContainer")
+    {
+        moc.classList.toggle("myOrderContainer");
+    }
 }
 
+function mostrarMyOrderContainer() {
+    moc.classList.toggle("myOrderContainer");
+    if (cm.className=="none menu-container")
+    {
+        cm.classList.toggle("menu-container");
+    }
 
+    if (cmv.className=="none menu-movile-container")
+    {
+        cmv.classList.toggle("menu-movile-container");
+    }
+}
