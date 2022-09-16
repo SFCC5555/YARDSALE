4a074a2efd12858908f739ef6f7c9c4a2e2a1fb5
sfcc5555 <sfcc.5555@gmail.com>
2022-09-15 22:27:41 -0500

    Actualización

--- static/YARDSALE/yardsale.js
+++ static/YARDSALE/yardsale.js
@@ -1,5 +1,13 @@
 let cm=document.getElementById("contenedorMenu");
+let cmv=document.getElementById("contenedorMenuMovile");
 
 function mostrarMenu() {
     cm.classList.toggle("menu-container")
 }
+
+
+function mostrarMenuMovile() {
+    cmv.classList.toggle("menu-movile-container")
+}
+
+
